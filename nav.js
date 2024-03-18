e7e5c433aeb0b8fc78b78da21d0715f3022ea76c
Tanmay337442 <tanmay337442@gmail.com>
2024-03-17 22:29:53 -0400

    test navbar link active state

--- nav.js
+++ nav.js
@@ -3,3 +3,5 @@ $(document).ready(function(){
     $(".nav-link").attr("class", "nav-link");
     $(this).attr("class", "nav-link active");
 });
+const element = document.getElementsByClassName("nav-link active");
+console.log(element);
