f1901703a93d3db7d5f1e5894862165e215b5738
Tanmay337442 <tanmay337442@gmail.com>
2024-03-18 18:11:51 -0400

    navbar active link please work now

--- nav.js
+++ nav.js
@@ -1,9 +1,5 @@
 $("#nav-placeholder").load("nav.html");
 
 $(document).ready(function(){
-    var pathname = window.location.pathname;
     $(".nav-link active").attr("class", "nav-link");
-    $("a[href='" + pathname + "']").attr("class", "nav-link active");
 });
-
-console.log(pathname)
