8b671f290d01ffdaa05312916fc5849f06836b63
Tanmay337442 <tanmay337442@gmail.com>
2024-03-17 22:04:25 -0400

    navbar links

--- nav.js
+++ nav.js
@@ -1,1 +1,4 @@
 $("#nav-placeholder").load("nav.html");
+var pathname = window.location.href;
+$(".nav-link").attr("class", "nav-link");
+$("a[href='" + pathname + "']").attr("class", "nav-link active");
