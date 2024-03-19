a2348d98d3797903da40bb850308155f43571a7a
Tanmay337442 <tanmay337442@gmail.com>
2024-03-19 18:58:08 -0400

    test

--- nav.js
+++ nav.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     var active = $('a.active');
     console.log(active);
     $('a.active').removeClass('active').removeAttr('aria-current');
+    active = $('a.active');
+    console.log(active);
     $('a[href="' + location.href + '"]').addClass('active').attr('aria-current', 'page');
     console.log(location.href);
 });
