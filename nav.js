6fa89446341dafe396bca6bd9b1d8ac6ea79db4e
Tanmay337442 <tanmay337442@gmail.com>
2024-03-19 19:32:52 -0400

    navbar test

--- nav.js
+++ nav.js
@@ -1,10 +1,8 @@
-$(document).ready(function() {
-    $("#nav-placeholder").load("nav.html");
-    var active = $('#nav-placeholder a.active');
-    console.log(active);
-    $('#nav-placeholder a.active').removeClass('active').removeAttr('aria-current');
-    active = $('#nav-placeholder a.active');
-    console.log(active);
-    $('a[href="' + location.href + '"]').addClass('active').attr('aria-current', 'page');
-    console.log(location.href);
-});
+$("#nav-placeholder").load("nav.html");
+var active = $('a.active');
+console.log(active);
+$('a.active').removeClass('active').removeAttr('aria-current');
+active = $('a.active');
+console.log(active);
+$('a[href="' + location.href + '"]').addClass('active').attr('aria-current', 'page');
+console.log(location.href);
