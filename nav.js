3c4d7c9eacd1550b8148fdbafd26c2bca98447d6
Tanmay337442 <tanmay337442@gmail.com>
2024-03-20 11:27:45 -0400

    no navbar active state

--- nav.js
+++ nav.js
@@ -1,7 +1,3 @@
 $(document).ready(function() {
     $("#nav-placeholder").load("nav.html");
-    $("#nav-placeholder").on("click", "a", function() {
-        $("#nav-placeholder a.active").removeClass("active");
-        $(this).addClass("active");
-    });
 });
