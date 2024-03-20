b4720d943ce1d9af012e995912d7a244dcef8122
Tanmay337442 <tanmay337442@gmail.com>
2024-03-19 20:10:22 -0400

    navbar test

--- nav.js
+++ nav.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $("#nav-placeholder").load("nav.html");
     $("#nav-placeholder").on("click", "a", function() {
         $("#nav-placeholder a.active").removeClass("active");
         $(this).addClass("active");
